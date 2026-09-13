cce8be1690836eddedc85b88660daa97dedffa73
NickPavlovskii <o_oniklado_o@list.ru>
2026-09-13 15:21:27 +0300

    докер

--- docker/render/env.sh
+++ docker/render/env.sh
@@ -14,6 +14,10 @@ if [ -z "$APP_URL" ] && [ -n "$RENDER_EXTERNAL_URL" ]; then
   export APP_URL="$RENDER_EXTERNAL_URL"
 fi
 
+if [ -z "$APP_URL" ] && [ -n "$RAILWAY_PUBLIC_DOMAIN" ]; then
+  export APP_URL="https://${RAILWAY_PUBLIC_DOMAIN}"
+fi
+
 if [ -z "$FRONTEND_URL" ] && [ -n "$APP_URL" ]; then
   export FRONTEND_URL="$APP_URL"
 fi
@@ -22,8 +26,16 @@ if [ -z "$SANCTUM_STATEFUL_DOMAINS" ] && [ -n "$RENDER_EXTERNAL_HOSTNAME" ]; the
   export SANCTUM_STATEFUL_DOMAINS="$RENDER_EXTERNAL_HOSTNAME"
 fi
 
+if [ -z "$SANCTUM_STATEFUL_DOMAINS" ] && [ -n "$RAILWAY_PUBLIC_DOMAIN" ]; then
+  export SANCTUM_STATEFUL_DOMAINS="$RAILWAY_PUBLIC_DOMAIN"
+fi
+
 if [ -z "$PARSER_URL" ] && [ -n "$PARSER_HOSTPORT" ]; then
   export PARSER_URL="http://${PARSER_HOSTPORT}"
+fi
+
+if [ -z "$PARSER_URL" ] && [ -n "$PARSER_PRIVATE_HOST" ]; then
+  export PARSER_URL="http://${PARSER_PRIVATE_HOST}:${PARSER_PRIVATE_PORT:-3000}"
 fi
 
 mkdir -p storage/framework/cache/data \
